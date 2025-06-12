5032bf29965153de4c1c0325faff1fee8fceda72
Yuanchen Sun <179019360+yuanchen798@users.noreply.github.com>
2025-06-12 23:04:45 +0200

    create demo_train and gaussian_head

--- vggt/models/vggt.py
+++ vggt/models/vggt.py
@@ -18,10 +18,13 @@ class VGGT(nn.Module, PyTorchModelHubMixin):
     def __init__(self, img_size=518, patch_size=14, embed_dim=1024):
         super().__init__()
 
+        self.embed_dim = embed_dim
         self.aggregator = Aggregator(img_size=img_size, patch_size=patch_size, embed_dim=embed_dim)
         self.camera_head = CameraHead(dim_in=2 * embed_dim)
         self.point_head = DPTHead(dim_in=2 * embed_dim, output_dim=4, activation="inv_log", conf_activation="expp1")
         self.depth_head = DPTHead(dim_in=2 * embed_dim, output_dim=2, activation="exp", conf_activation="expp1")
+        # 这里以后的output_dim以后需要变成gaussian_num_channels
+        self.gaussian_head = DPTHead(dim_in=2 * embed_dim, output_dim=14, activation="exp", conf_activation="expp1")
         self.track_head = TrackHead(dim_in=2 * embed_dim, patch_size=patch_size)
 
     def forward(
